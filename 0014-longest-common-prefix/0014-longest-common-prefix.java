feaac3b734806ef6bd0995ac441ee6fccd062ac7
zgfhzg <43362359+zgfhzg@users.noreply.github.com>
2024-11-29 14:47:50 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        String result = "";
+        String tmp = strs[0];
+
+        if (strs.length == 1) {
+            return tmp;
+        }
+
+        for (int j = 0; j < tmp.length(); j++) {
+            boolean flag = true;
+            for (int i = 1; i < strs.length; i++) {
+                if (!strs[i].startsWith(tmp.substring(0, j))) {
+                    flag = false;
+                }
+            }
+            if (flag) {
+                result = tmp.substring(0, j);
+            }
+        }
+        return result;
+    }
+}
