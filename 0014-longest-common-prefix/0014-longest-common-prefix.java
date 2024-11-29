870291e50ef89f58dd40899bac9503f5c79c46c9
zgfhzg <43362359+zgfhzg@users.noreply.github.com>
2024-11-29 14:56:43 +0900

    Time: 3 ms (17.71%), Space: 44.1 MB (5.07%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -9,14 +9,13 @@ class Solution {
 
         for (int j = 0; j <= tmp.length(); j++) {
             boolean flag = true;
-            String str = tmp.substring(0, j);
             for (int i = 1; i < strs.length; i++) {
-                if (strs[i].indexOf(str) != 0) {
+                if (!strs[i].startsWith(tmp.substring(0, j))) {
                     flag = false;
                 }
             }
             if (flag) {
-                result = str;
+                result = tmp.substring(0, j);
             }
         }
         return result;
