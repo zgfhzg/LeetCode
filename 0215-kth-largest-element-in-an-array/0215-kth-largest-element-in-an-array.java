f4656ba595279173d880409c6acf2399366221b7
zgfhzg <43362359+zgfhzg@users.noreply.github.com>
2025-07-10 10:20:56 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,12 +1,40 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue();
+        // PriorityQueue<Integer> pq = new PriorityQueue();
 
-        for (int i : nums) {
-            pq.offer(i);
+        // for (int i : nums) {
+        //     pq.offer(i);
 
-            if (pq.size() > k) pq.poll();
+        //     if (pq.size() > k) pq.poll();
+        // }
+        // return pq.poll();
+        quickSort(nums, 0, nums.length - 1);
+        return nums[nums.length - 1 - (k - 1)];
+    }
+
+    public void quickSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int pivot = pivot(arr, left, right);
+            quickSort(arr, left, pivot - 1);
+            quickSort(arr, pivot + 1, right);
         }
-        return pq.poll();
+    }
+
+    public int pivot(int[] arr, int left, int right) {
+        int pivot = arr[right];
+        int i = left - 1;
+
+        for (int j = left; j < right; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[right];
+        arr[right] = temp;
+        return i + 1;
     }
 }
